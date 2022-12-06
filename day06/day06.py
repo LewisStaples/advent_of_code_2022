583dd21618865332b40a2ac5700b5622e1453ce2
Lewis Staples <lewis.staples@gmail.com>
2022-12-06 00:20:49 -0500

    Day 06 Part B working

--- day06/day06.py
+++ day06/day06.py
@@ -8,13 +8,22 @@ print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     in_string = f.readline().rstrip()
     print(f'{in_string}\n')
-i = 4
+
+i_start_packet = 4
 while True:
-    potential_substring = in_string[i-4:i]
+    potential_substring = in_string[i_start_packet-4:i_start_packet]
     if len(set(potential_substring)) < 4:
-        i += 1
+        i_start_packet += 1
     else:
         break
-print(f'The answer to A is {i}\n')
+print(f'The answer to A is {i_start_packet}\n')
 
+i_start_message = 14
+while True:
+    potential_substring = in_string[i_start_message-14:i_start_message]
+    if len(set(potential_substring)) < 14:
+        i_start_message += 1
+    else:
+        break
+print(f'The answer to B is {i_start_message}\n')
 
