5dadc061f68c4c8ba54b2da86ac068f3d96ea2e6
Lewis Staples <lewis.staples@gmail.com>
2022-12-07 00:34:59 -0500

    Day 07 Succesfully calling cd/

--- day07/day07.py
+++ day07/day07.py
@@ -15,14 +15,14 @@ class directory_structure:
             if self.current_directory is None:
                 raise ValueError('You must run cd / before changing directory to anywhere else')
         raise(f'More code to be written to handle: cd {directory_param}')
+
+
 ds = directory_structure()
 
 # Reading input from the input file
 input_filename='input_sample0.txt'
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
-    # Pull in each line from the input file
-    # for line_num, in_string in enumerate(f):
     for in_string in f:
         in_string = in_string.rstrip()
 
@@ -31,7 +31,9 @@ with open(input_filename) as f:
         # Handling a command passed to command line
         if in_string[:2] == '$ ':
             command_str, params_str = in_string[2:].split(' ')
-            globals()[command_str](params_str)
+            # globals()[command_str](params_str)
+            method_to_run = getattr(ds, command_str)
+            method_to_run(params_str)
 
 
 
