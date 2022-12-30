cbda964dc1693ed550ac45e69e2ad53d7ede0e40
Lewis Staples <lewis.staples@gmail.com>
2022-12-30 10:35:56 -0500

    Day 23 Part A working

--- day23/day23.py
+++ day23/day23.py
@@ -17,14 +17,19 @@ def get_input(input_filename):
                     elf_map.add((x,y))
     return elf_map
 
-def display_elf_map(elf_map, map_title):
-    print(map_title)
+def get_margins(elf_map):
     margins = {'top': float('inf'), 'right': float('-inf'), 'bottom': float('-inf'), 'left': float('inf')}
     for point in elf_map:
         margins['top'] = min(margins['top'], point[1])
         margins['right'] = max(margins['right'], point[0])
         margins['bottom'] = max(margins['bottom'], point[1])
         margins['left'] = min(margins['left'], point[0])
+    return margins
+
+
+def display_elf_map(elf_map, map_title):
+    print(map_title)
+    margins = get_margins(elf_map)
     for y in range(margins['top'], margins['bottom'] + 1):
         for x in range(margins['left'], margins['right'] + 1):
             if (x,y) in elf_map:
@@ -38,8 +43,11 @@ def display_elf_map(elf_map, map_title):
 def required_vacancies_not_present(required_vacancies, elf_map):
     for vacancy in required_vacancies:
         if vacancy in elf_map:
-            # Try a different i value
+            # Found one instance where the requirement is not met
+            # One elf has been found to be present where a vacancy is required
+            # (Note that there might be others, too)
             return True
+    # The requirements are met: the required vacancies are all vacant
     return False
 
 
@@ -113,13 +121,12 @@ def make_moves(elf_map, round_number):
             if required_vacancies_not_present(required_vacancies, elf_map):
                 continue
             else:
-                # break
-
                 # Check if that proposed destination is already in proposed_moves
-                # If yes, mark that proposed_move as unreachable with a None (it's a flag)
                 if proposed_destination in proposed_moves:
+                    # If it's already there, mark that proposed_move as unreachable with a None (it's a flag)
                     proposed_moves[proposed_destination] = None
                 else:
+                    # This proposed_destination hasn't been seen before, so list it as a proposed_move
                     proposed_moves[proposed_destination] = tuple(elf_location)
                 break
 
@@ -128,25 +135,31 @@ def make_moves(elf_map, round_number):
 
     elf_map -= set(proposed_moves_pruned.values())
     elf_map.update(proposed_moves_pruned.keys())
-    dummy = 123
 
-
+def get_empty_tile_count(elf_map):
+    margins = get_margins(elf_map)
+    total_tile_count = ( margins['bottom'] - margins['top'] + 1) * ( margins['right'] - margins['left'] + 1 )
+    empty_tile_count = total_tile_count - len(elf_map)
+    return empty_tile_count
 
 def do_rounds(elf_map):
-    for round_number in range(1, 4):
+    for round_number in range(1, 11):
         make_moves(elf_map, round_number)
-        display_elf_map(elf_map, f'Elf map after round {round_number}')
+        # display_elf_map(elf_map, f'Elf map after round {round_number}')
+
+    empty_tile_count = get_empty_tile_count(elf_map)
+    print(f'The answer to part A is {empty_tile_count}\n')
+
+
 
 
 
 def solve_problem(input_filename):
     elf_map = get_input(input_filename)
-    display_elf_map(elf_map, 'Initial elf map')
     do_rounds(elf_map)
 
 
 solve_problem('input_sample1.txt')
 
-# def test_sample_0():
-#     solve_problem('input_sample0.txt')
+
     
